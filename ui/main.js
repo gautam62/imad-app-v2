f5046e4d98bbe3d2229e68994927d90350308765
gautam62 <gautam.aggarwal1208@gmail.com>
2017-03-25 01:10:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,25 +1,30 @@
 //counter code
-var button= document.getElementById('counter');
+var button = document.getElementById('counter');
+var counter= 0;
 
 button.onClick= function(){
     
     //Create q request object
-    var request= new XMLHttpRequest();
+    //var request= new XMLHttpRequest();
     
     //caoture the response and store it in a variable
-    request.onreadystatechange = function(){
-      if (request.readyState === XMLHttpRequest.DONE){
+   // request.onreadystatechange = function(){
+     // if (request.readyState === XMLHttpRequest.DONE){
           //take some action
-          if(request.status === 200){
-              var counter = request.responseText;
-                var span= document.getElementById('count');
-                span.innerHTML= counter.toString();
-          }
-      }  
+      //    if(request.status === 200){
+       //       var counter = request.responseText;
+       //         var span= document.getElementById('count');
+        //        span.innerHTML= counter.toString();
+       //   }
+    //  }  
       //Not done yet
-    };
+  //  };
     
     //Make the request
-    request.open('GET','http://gautam62.imad.hasura-app.io/counter',true);
-    request.send(null);
+  //  request.open('GET','http://gautam62.imad.hasura-app.io/counter',true);
+   // request.send(null);
+   
+   counter = counter + 1;
+   var span= document.getElementById('count');
+   span.innerHTML= counter.toString();
 };
